3979275ae9da795032efbf3519faa3055f249f6b
Basit rehman <basitrehman696969@gmail.com>
2026-04-02 16:28:25 +0500

    Fix data.js export and Project.jsx import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './pages/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
-import Skill from './pages/skill.jsx'
+import Skill from './pages/Skill.jsx'
 import Project from './pages/Project.jsx'
 import Footer from './pages/Footer.jsx'
 
